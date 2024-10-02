74f5b94643d1ed6305db60ce6c6238fabf0f4cc9
lobna <lobnaabdelrhman89@gmail.com>
2024-10-02 15:54:05 +0300

    Resolved merge conflict and added new image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  jopCount: number = 0; // starting number
+  finalJopCount: number = 200000; // target number
+  incrementSpeed: number = 20; // speed of incrementing (in milliseconds)
 
+  ngOnInit(): void {
+    this.startCounter();
+  }
+
+  startCounter(){
+    let interval = setInterval(() => {
+      if (this.jopCount < this.finalJopCount) {
+        this.jopCount += Math.ceil(this.finalJopCount / 100); // adjust the increment step
+      } else{
+        this.jopCount = this.finalJopCount; // stop at the final count
+        clearInterval(interval);
+      }
+    }, this.incrementSpeed);
+  }
 }
