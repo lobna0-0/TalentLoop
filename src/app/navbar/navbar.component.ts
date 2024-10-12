9ca315b333549f6f9a3eab8fbf1cd070ceec9b08
Basant-Adel <basantadel99@gmail.com>
2024-10-12 17:40:00 +0200

    revert: Implement a sticky navbar with dynamic scrolling and style updates

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,21 +1,10 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+export class NavbarComponent {
 
-export class NavbarComponent implements OnInit {
-  isSticky = false;
-
-  headerHeight = 600;
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    this.isSticky = scrollPosition > this.headerHeight;
-  }
-
-  ngOnInit(): void {}
 }
